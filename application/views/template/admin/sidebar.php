7e9285660c328d3bdefeda6b17cbbfb6bb2f619b
JeanMa <cjeanma009@gmail.com>
2020-02-20 10:43:07 -0500

    modificacion del menu

--- application/views/template/admin/sidebar.php
+++ application/views/template/admin/sidebar.php
@@ -4,9 +4,9 @@
       <!-- Sidebar - Brand -->
       <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
         <div class="sidebar-brand-icon rotate-n-15">
-          <i class="fas fa-laugh-wink"></i>
+          <i class="fas fa-book"></i>
         </div>
-        <div class="sidebar-brand-text mx-3">SB Admin <sup>2</sup></div>
+        <div class="sidebar-brand-text mx-3">IESTP-ILAVE</div>
       </a>
 
       <!-- Divider -->
@@ -24,37 +24,35 @@
 
       <!-- Heading -->
       <div class="sidebar-heading">
-        Interface
+        Personas
       </div>
 
       <!-- Nav Item - Pages Collapse Menu -->
       <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
-          <i class="fas fa-fw fa-cog"></i>
-          <span>Components</span>
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#optEstudiantes" aria-expanded="true" aria-controls="optEstudiantes">
+          <i class="fas fa-user"></i>
+          <span>Estudiantes</span>
         </a>
-        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+        <div id="optEstudiantes" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
           <div class="bg-white py-2 collapse-inner rounded">
-            <h6 class="collapse-header">Custom Components:</h6>
-            <a class="collapse-item" href="buttons.html">Buttons</a>
-            <a class="collapse-item" href="cards.html">Cards</a>
+            <h6 class="collapse-header">Opciones: </h6>
+            <a class="collapse-item" href="estudiante">Lista Estudiantes</a>
+            <a class="collapse-item" href="estudiante/add">Nuevo Estudiante</a>
           </div>
         </div>
       </li>
 
       <!-- Nav Item - Utilities Collapse Menu -->
       <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities" aria-expanded="true" aria-controls="collapseUtilities">
-          <i class="fas fa-fw fa-wrench"></i>
-          <span>Utilities</span>
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#optDocentes" aria-expanded="true" aria-controls="optDocentes">
+          <i class="fas fa-user"></i>
+          <span>Docentes</span>
         </a>
-        <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
+        <div id="optDocentes" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
           <div class="bg-white py-2 collapse-inner rounded">
-            <h6 class="collapse-header">Custom Utilities:</h6>
-            <a class="collapse-item" href="utilities-color.html">Colors</a>
-            <a class="collapse-item" href="utilities-border.html">Borders</a>
-            <a class="collapse-item" href="utilities-animation.html">Animations</a>
-            <a class="collapse-item" href="utilities-other.html">Other</a>
+            <h6 class="collapse-header">Opciones</h6>
+            <a class="collapse-item" href="docente">Lista Docentes</a>
+            <a class="collapse-item" href="docente/add">Nuevo Docente</a>
           </div>
         </div>
       </li>
@@ -64,45 +62,98 @@
 
       <!-- Heading -->
       <div class="sidebar-heading">
-        Addons
+        Procedimientos
       </div>
 
       <!-- Nav Item - Pages Collapse Menu -->
       <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true" aria-controls="collapsePages">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#optMatriculas" aria-expanded="true" aria-controls="optMatriculas">
           <i class="fas fa-fw fa-folder"></i>
-          <span>Pages</span>
+          <span>Matriculas</span>
         </a>
-        <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
+        <div id="optMatriculas" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
           <div class="bg-white py-2 collapse-inner rounded">
-            <h6 class="collapse-header">Login Screens:</h6>
-            <a class="collapse-item" href="login.html">Login</a>
-            <a class="collapse-item" href="register.html">Register</a>
-            <a class="collapse-item" href="forgot-password.html">Forgot Password</a>
-            <div class="collapse-divider"></div>
-            <h6 class="collapse-header">Other Pages:</h6>
-            <a class="collapse-item" href="404.html">404 Page</a>
-            <a class="collapse-item" href="blank.html">Blank Page</a>
-          </div>
+            <h6 class="collapse-header">Opciones</h6>
+            <a class="collapse-item" href="matricula/regular">Matricula Regular</a>
+            <a class="collapse-item" href="matricula/recuperacion">Matricula Recuperacion</a>
+            <a class="collapse-item" href="matricula/extraordinaria">Matricula Extraordinaria</a>
         </div>
       </li>
 
       <!-- Nav Item - Charts -->
       <li class="nav-item">
-        <a class="nav-link" href="charts.html">
-          <i class="fas fa-fw fa-chart-area"></i>
-          <span>Charts</span></a>
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#optPracticas" aria-expanded="true" aria-controls="optPracticas">
+          <i class="fas fa-fw fa-folder"></i>
+          <span>Practicas Pre Prof.</span>
+        </a>
+        <div id="optPracticas" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
+          <div class="bg-white py-2 collapse-inner rounded">
+            <h6 class="collapse-header">Opciones</h6>
+            <a class="collapse-item" href="practica">Lista Practicas</a>
+            <a class="collapse-item" href="practica/add">Nueva Practica</a>
+        </div>
       </li>
 
       <!-- Nav Item - Tables -->
       <li class="nav-item">
-        <a class="nav-link" href="tables.html">
+        <a class="nav-link" href="certificados">
           <i class="fas fa-fw fa-table"></i>
-          <span>Tables</span></a>
+          <span>Certificados y Constancias</span></a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#optPlazos" aria-expanded="true" aria-controls="optPlazos">
+          <i class="fas fa-fw fa-folder"></i>
+          <span>Plazos</span>
+        </a>
+        <div id="optPlazos" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
+          <div class="bg-white py-2 collapse-inner rounded">
+            <h6 class="collapse-header">Opciones</h6>
+            <a class="collapse-item" href="plazomatricula">Plazos Matricula</a>
+            <a class="collapse-item" href="plazonota">Plazos Notas</a>
+        </div>
       </li>
 
       <!-- Divider -->
       <hr class="sidebar-divider d-none d-md-block">
+
+      <!-- Heading -->
+      <div class="sidebar-heading">
+        Administracion
+      </div>
+
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#optProgramas" aria-expanded="true" aria-controls="optProgramas">
+          <i class="fas fa-fw fa-folder"></i>
+          <span>Programas de Estudio</span>
+        </a>
+        <div id="optProgramas" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
+          <div class="bg-white py-2 collapse-inner rounded">
+            <h6 class="collapse-header">Opciones</h6>
+            <a class="collapse-item" href="escuelaprofesional">Lista Programas</a>
+            <a class="collapse-item" href="escuelaprofesional/add">Nuevo Programa</a>
+        </div>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#optUsuarios" aria-expanded="true" aria-controls="optUsuarios">
+          <i class="fas fa-fw fa-folder"></i>
+          <span>Usuarios</span>
+        </a>
+        <div id="optUsuarios" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
+          <div class="bg-white py-2 collapse-inner rounded">
+            <h6 class="collapse-header">Opciones</h6>
+            <a class="collapse-item" href="usuario">Lista Usuarios</a>
+            <a class="collapse-item" href="usuario/add">Nuevo Usuario</a>
+        </div>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link" href="reportes">
+          <i class="fas fa-fw fa-table"></i>
+          <span>Reportes</span></a>
+      </li>
+
 
       <!-- Sidebar Toggler (Sidebar) -->
       <div class="text-center d-none d-md-inline">
